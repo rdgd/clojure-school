be69a15942e3f55982b6beb2d95a0f5cb2a3fdc0
rdgd <ryan.is.gray@gmail.com>
2020-10-23 13:30:12 -0500

    Added some more introductory exercises.

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -2,8 +2,33 @@
   (:require [clojure.test :as t]
             [clojure.repl :refer :all]))
 
-(defn add [a b] (+ a b))
+; Functions and defs
+(def my-name "General Kenobi")
 
-(defn -main [& args] (println "hooray!"))
+; This is a named function, which uses the defn macro. You will use this a lot!
+(defn greeting [n] (str n ", you are a bold one!"))
 
-(+ 1 2) ;Put your cursor over the opening paren and type ctrl + return
+; Writing a function like above, is equivalent to...
+(def greeting2 (fn [n] (str n ", you are a bold one!")))
+
+; This is also equivalent to...
+(def greeting3 #(str %1 ", you are a bold one!"))
+
+; This is because defn is a macro, which gives us sugar around def'ing a function
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise
+; Put your cursor over the opening paren and evaluate the expression (in VS Code it's: ctrl + return)
+(+ 1 2) ; => 3
+
+; Since we have the clojure.repl namespace loaded, we can look up the documentation as well as the source code for
+; + the function we just used
+; (doc +)
+; (source +)
+
+; See if you can implement the function add, uncomment the line below, invoke it, and observe the output
+;(add 1 2) ; => 3
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn -main [& args] (println "hooray and welcome!"))
